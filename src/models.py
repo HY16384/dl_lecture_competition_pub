66a66aa04b89807a62639fd155f0a71ce84cb212
24yamada <s7353109@g.ecc.u-tokyo.ac.jp>
2024-07-14 13:16:25 +0900

    added wavenet

--- src/models.py
+++ src/models.py
@@ -10,19 +10,22 @@ class BasicConvClassifier(nn.Module):
         num_classes: int,
         seq_len: int,
         in_channels: int,
-        hid_dim: int = 128
+        hid_dim: int = 128,
+        kernel_size = 3
     ) -> None:
         super().__init__()
 
         self.blocks = nn.Sequential(
-            ConvBlock(in_channels, hid_dim),
-            ConvBlock(hid_dim, hid_dim),
+            wave_block1 = Wave_Block(in_channels, 16, 12, kernel_size),
+            wave_block2 = Wave_Block(16, 32, 8, kernel_size),
+            wave_block3 = Wave_Block(32, 64, 4, kernel_size),
+            wave_block4 = Wave_Block(64, 128, 1, kernel_size)
         )
 
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool1d(1),
             Rearrange("b d 1 -> b d"),
-            nn.Linear(hid_dim, num_classes),
+            nn.Linear(128, num_classes),
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
@@ -36,6 +39,34 @@ class BasicConvClassifier(nn.Module):
 
         return self.head(X)
 
+# from https://www.kaggle.com/code/cswwp347724/wavenet-pytorch
+class Wave_Block(nn.Module):
+
+    def __init__(self, in_channels, out_channels, dilation_rates, kernel_size):
+        super(Wave_Block, self).__init__()
+        self.num_rates = dilation_rates
+        self.convs = nn.ModuleList()
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
+
+        self.convs.append(nn.Conv1d(in_channels, out_channels, kernel_size=1))
+        dilation_rates = [2 ** i for i in range(dilation_rates)]
+        for dilation_rate in dilation_rates:
+            self.filter_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
+            self.gate_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
+            self.convs.append(nn.Conv1d(out_channels, out_channels, kernel_size=1))
+
+    def forward(self, x):
+        x = self.convs[0](x)
+        res = x
+        for i in range(self.num_rates):
+            x = torch.tanh(self.filter_convs[i](x)) * torch.sigmoid(self.gate_convs[i](x))
+            x = self.convs[i + 1](x)
+            res = res + x
+        return res
+    
 
 class ConvBlock(nn.Module):
     def __init__(
